9f4c76cffaadaa131351a35d960a2570035e3f15
Lijing <lijing@camera360.com>
2015-06-02 03:06:52 +0800

    漏掉的文件

---
+++ AAAcounter/Model/Table/Place.cs
@@ -0,0 +1,35 @@
+﻿/* *
+* ClassName: Place
+* Description: 
+*
+* Author: 李靖
+* Date: 5/28/2015 11:07:41 PM
+* */
+
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAAcounter.Model
+{
+    public class Place : ITable
+    {
+        [AutoIncrement]
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [Ignore]
+        public string ChangedProp { get; set; }
+
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitute { get; set; }
+
+        public string City { get; set; }
+    }
+}
